b07b66049791b5419db60c01d5ac744750f43223
GFSA66 <gf66sorokina@gmail.com>
2024-08-18 15:56:47 +0300

    1.2.0

--- client/app.js
+++ client/app.js
@@ -7,7 +7,7 @@ const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('src'));
+app.use(express.static('public'));
 
 app.get('/',(req,res) =>{
     res.sendFile(path.join(__dirname,'public','index.html'));
